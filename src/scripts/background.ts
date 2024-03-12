0acdc17ff911906b15076d7129011048bd6dd0c4
ramilsaavedra <ramilsmcl@gmail.com>
2024-03-12 12:48:44 +0800

    feat: get audio quality / update track details value

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,22 +1,3 @@
-import { runtime } from "webpack"
-
 chrome.runtime.onInstalled.addListener(async () => {
   console.log("Tidal Discord Rich Pressence successfully installed")
-})
-
-// keep the sw alive
-chrome.runtime.onConnect.addListener((port) => {
-  if (port.name !== "tidal-content-script") {
-    return
-  }
-
-  port.onMessage.addListener((msg: unknown, port: chrome.runtime.Port) => {
-    console.log(`Message: ${msg} | Port Name: ${port.name}`)
-  })
-
-  port.onDisconnect.addListener((port: chrome.runtime.Port) => {
-    console.log(`Disconnected ${port.name}`)
-  })
-
-  port.postMessage("hi from service worker")
 })
--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,26 +1,61 @@
 const targetNode = document.getElementById("wimp")
+
 let title: Element
 let artists: Element
 let albumImg: Element
 let playBtn: Element
+let qualityElem: Element
 let once = 1
+
+let songTitle: string
+let artistsName: string
+let albumImgUrl: string
+let playBtnState = false
+let quality: string
 
 // @ts-ignore
 // Callback function to execute when mutations are observed
 const callback = (mutationList, observer) => {
   for (const mutation of mutationList) {
-    if (title === mutation.target) {
-      console.log("im the title")
-    }
-    if (mutation.type === "childList") {
-      if (once === 1) {
+    // get the initial element
+    if (once === 1) {
+      if (mutation.type === "childList") {
         getTrackElements()
         once++
+        mutationConsoleHandler()
         return
       }
     }
 
-    mutationConsoleHandler(mutation)
+    if (mutation.type === "childList") {
+      // check if there is a changed on artist
+      if (mutation.target === artists) {
+        artistsName = artistsNameHandler(Array.from(artists.childNodes))
+      }
+
+      // check if there si a changed on quality
+      if (mutation.target === qualityElem) {
+        quality = qualityElem.textContent
+      }
+    }
+
+    // check if there si a changed on title
+    if (mutation.type === "characterData") {
+      songTitle = title.textContent
+    }
+
+    if (mutation.type === "attributes") {
+      // check the if paused or playing
+      if (mutation.target === playBtn) {
+        playBtnState = !playBtn.getAttribute("href").includes("_play")
+      }
+
+      if (mutation.target === albumImg) {
+        albumImgUrl = albumImg.getAttribute("src")
+      }
+    }
+
+    mutationConsoleHandler()
   }
 }
 
@@ -36,34 +71,42 @@ observer.observe(targetNode, config)
 function getTrackElements() {
   try {
     title = document.querySelector(".wave-text-description-demi")
+    songTitle = title.textContent
 
     artists = document.querySelector("#footerPlayer .artist-link")
+    artistsName = artistsNameHandler(Array.from(artists.childNodes))
 
     albumImg = document.querySelector("figure[data-test] img")
+    albumImgUrl = albumImg.getAttribute("src")
 
-    playBtn = document.querySelector("#playbackControlBar > div > button")
+    playBtn = document.querySelector(
+      "#playbackControlBar > div > button > svg > use"
+    )
+
+    qualityElem = document.querySelector(
+      "button[data-test-media-state-indicator-streaming-quality] > span"
+    )
+
+    quality = qualityElem.textContent
 
     observer.observe(title, { characterData: true, subtree: true })
     observer.observe(artists, { childList: true })
     observer.observe(albumImg, { attributes: true })
-    // observer.observe(playBtn, { childList: true })
+    observer.observe(playBtn, {
+      attributes: true,
+    })
+    observer.observe(qualityElem, { characterData: true, subtree: true })
   } catch (error) {
-    console.log(error.Message)
+    console.log(error.message)
   }
 }
 
-function mutationConsoleHandler(mutation: any) {
-  switch (mutation.type) {
-    case "attributes":
-      console.log(`Album Image Source: ${mutation.target.src}`)
-      return
-    case "childList":
-      let artistsName = Array.from(mutation.target.childNodes)
-        .map((artist: Element) => artist.textContent)
-        .join(", ")
-      console.log(`Artists: ${artistsName}`)
-      return
-    case "characterData":
-      console.log(`Title: ${mutation.target.textContent}`)
-  }
+function artistsNameHandler(artists: ChildNode[]) {
+  return artists.map((artist: Element) => artist.textContent).join(", ")
+}
+
+function mutationConsoleHandler() {
+  console.log(
+    `${songTitle} by ${artistsName} | album image src: ${albumImgUrl} | currently playing: ${playBtnState} | quality: ${quality}`
+  )
 }
